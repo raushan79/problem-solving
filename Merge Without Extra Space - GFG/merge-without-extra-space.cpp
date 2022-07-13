e49550aa4abe53036aaee72672d55fe5348c8a6f
RAUSHAN KUMAR <raushansft@gmail.com>
2022-07-14 00:07:27 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -1,14 +1,16 @@
 // { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> 
+using namespace std; 
+
 
  // } Driver Code Ends
-//User function template for C++
 class Solution{
-public:
-	void merge(int arr1[], int arr2[], int n, int m) {
-	    // code here
- int s=n+m;
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            // code here 
+             int s=n+m;
 	    int ans[s];
 	    int left=0;
 	    int right=0;
@@ -49,30 +51,40 @@ public:
 	       z++;
 	   }
 	}
-};
-// { Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m, i;
-        cin >> n >> m;
-        int arr1[n], arr2[m];
-        for (i = 0; i < n; i++) {
-            cin >> arr1[i];
-        }
-        for (i = 0; i < m; i++) {
-            cin >> arr2[i];
-        }
-        Solution ob;
-        ob.merge(arr1, arr2, n, m);
-        for (i = 0; i < n; i++) {
-            cout << arr1[i] << " ";
-        }
-        for (i = 0; i < m; i++) {
-            cout << arr2[i] << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}  // } Driver Code Ends
+};// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
